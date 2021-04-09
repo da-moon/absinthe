742c0a06d0d0f33b5ce775546de65407c30af3f8
da-moon <damoon.azarpazhooh@live.com>
2021-04-09 17:20:59 -0400

    fixed getopt flag validation bug

--- lib/getopt.ps1
+++ lib/getopt.ps1
@@ -1,13 +1,13 @@
-# adapted from http://hg.python.org/cpython/file/2.7/Lib/getopt.py
-# argv:
-#    array of arguments
-# shortopts:
-#    string of single-letter options. options that take a parameter
-#    should be follow by ':'
-# longopts:
-#    array of strings that are long-form options. options that take
-#    a parameter should end with '='
-# returns @(opts hash, remaining_args array, error string)
+# ────────────────────────────────────────────────────────────────────────────────
+# snippet to source this script:
+#
+#   $getopt_url = 'https://raw.githubusercontent.com/da-moon/psutils/master/lib/getopt.ps1'
+#   Invoke-Expression (New-Object net.webclient).downloadstring($getopt_url)
+# ────────────────────────────────────────────────────────────────────────────────
+
+# [ NOTE ] =>
+# https://github.com/lukesampson/psutils/blob/master/getopt.ps1
+
 function getopt($argv, $shortopts, $longopts) {
     $opts = @{}; $rem = @()
 
@@ -18,55 +18,84 @@ function getopt($argv, $shortopts, $longopts) {
     function regex_escape($str) {
         return [regex]::escape($str)
     }
+    function  validate_argv([Object] $argv) {
+        $argv_clone = $argv[$i + 1]
+        if (($argv_clone -is [int]) -or ($argv_clone -is [decimal])) { 
+            $argv_clone = $argv_clone.ToString()
+        }
+        if (($argv_clone.startswith('--')) -or ($argv_clone.startswith('-'))) {
+            return $false
+        }
+        return $true
 
+    } 
     # ensure these are arrays
     $argv = @($argv)
     $longopts = @($longopts)
 
-    for($i = 0; $i -lt $argv.length; $i++) {
+    for ($i = 0; $i -lt $argv.length; $i++) {
         $arg = $argv[$i]
-        if($null -eq $arg) { continue }
+        if ($null -eq $arg) { continue }
+
         # don't try to parse array arguments
-        if($arg -is [array]) { $rem += ,$arg; continue }
-        if($arg -is [int]) { $rem += $arg; continue }
-        if($arg -is [decimal]) { $rem += $arg; continue }
+        if ($arg -is [array]) { $rem += , $arg; continue }
+        if ($arg -is [int]) { $rem += $arg; continue }
+        if ($arg -is [decimal]) { $rem += $arg; continue }
 
-        if($arg.startswith('--')) {
+        if ($arg.startswith('--')) {
             $name = $arg.substring(2)
-
+            
             $longopt = $longopts | Where-Object { $_ -match "^$name=?$" }
 
-            if($longopt) {
-                if($longopt.endswith('=')) { # requires arg
-                    if($i -eq $argv.length - 1) {
+            if ($longopt) {
+
+
+                if ($longopt.endswith('=')) {
+                    # requires arg
+                    if ($i -eq $argv.length - 1) {
                         return err "Option --$name requires an argument."
                     }
+                    if (-not(validate_argv ($argv))) {
+                        $faulty_arg = $argv[($i + 1)]
+                        return err "Option --$name got an invalid argument: [ $faulty_arg ]"
+                    }
                     $opts.$name = $argv[++$i]
-                } else {
+                }
+                else {
                     $opts.$name = $true
                 }
-            } else {
+            }
+            else {
                 return err "Option --$name not recognized."
             }
-        } elseif($arg.startswith('-') -and $arg -ne '-') {
-            for($j = 1; $j -lt $arg.length; $j++) {
+        }
+        elseif ($arg.startswith('-') -and $arg -ne '-') {
+            for ($j = 1; $j -lt $arg.length; $j++) {
                 $letter = $arg[$j].tostring()
 
-                if($shortopts -match "$(regex_escape $letter)`:?") {
+                if ($shortopts -match "$(regex_escape $letter)`:?") {
                     $shortopt = $matches[0]
-                    if($shortopt[1] -eq ':') {
-                        if($j -ne $arg.length -1 -or $i -eq $argv.length - 1) {
+                    if ($shortopt[1] -eq ':') {
+
+                        if ($j -ne $arg.length - 1 -or $i -eq $argv.length - 1) {
                             return err "Option -$letter requires an argument."
                         }
+                        if (-not(validate_argv ($argv))) {
+                            $faulty_arg = $argv[($i + 1)]
+                            return err "Option --$name got an invalid argument: [ $faulty_arg ]"
+                        }
                         $opts.$letter = $argv[++$i]
-                    } else {
+                    }
+                    else {
                         $opts.$letter = $true
                     }
-                } else {
+                }
+                else {
                     return err "Option -$letter not recognized."
                 }
             }
-        } else {
+        }
+        else {
             $rem += $arg
         }
     }
